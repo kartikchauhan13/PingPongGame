5ec14879fd5de17b5b31c62d8c5928e312c04d8f
kartikchauhan13 <101096226+kartikchauhan13@users.noreply.github.com>
2022-04-04 14:20:54 +0530

    keydown

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,14 @@
 import pygame
 from pygame.locals import *
+import time
 
 if __name__=="__main__":
 	pygame.init()
 
 	surface=pygame.display.set_mode((500,500))
+	flag=True
+	while flag:
+		for event in pygame.event.get():
+			if event.type ==KEYDOWN :
+				if event.type ==K_ESCAPE:
+					
